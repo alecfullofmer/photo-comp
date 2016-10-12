9cba9aad70f0fd5b944b305d16dfee5211afb855
alecfullofmer <arfullmer@verisk.com>
2016-10-12 16:26:52 -0600

    photo component and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PhotoViewerComponent } from './photo-viewer/photo-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoViewerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'photos',
+        component: PhotoViewerComponent
+      },
+      { path: '', redirectTo: '/photos', pathMatch: 'full' },
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
